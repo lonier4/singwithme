73c75f16fce337b96ba93d31cb7f61ad1022c699
Andre Yeargins <lonier4@gmail.com>
2020-01-25 19:16:11 +0000

    testing each for nil class

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,31 @@
+class VideosController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @videos = Video.all
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = current_user.videos.create(video_params)
+    if @video.valid?
+      redirect_to video_path(@video)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :artist)
+  end
+
+end
